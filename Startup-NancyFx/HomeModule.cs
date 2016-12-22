587963a6c6817d7fca32480ba37a67ab5b71c46f
daijinming <daijinming@live.cn>
2016-12-22 10:12:19 +0800

    Startup-NancyFx第一次提交
    Startup-NancyFx第一次提交

---
+++ Startup-NancyFx/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Startup_NancyFx
+{
+    public class HomeModule:NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ =>
+            {   
+                return this.Response.AsText("Hello Nancy", "text/html");
+            }; 
+        }
+    }
+}
---
+++ Startup-NancyFx/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Startup_NancyFx.Startup))]
+namespace Startup_NancyFx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            
+            app.UseNancy();
+        }
+
+    }
+}
